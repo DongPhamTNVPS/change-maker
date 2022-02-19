c0edab5244a62575ba6c49da31c9dbed46b581d5
DongPhamTNVPS <vandong875@gmail.com>
2022-02-19 18:23:06 +0700

    first commit

---
+++ app/build/generated/source/buildConfig/debug/com/dong_pham/phamhavandong_19500311_changemaker/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.dong_pham.phamhavandong_19500311_changemaker;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "iuh_ad.phamthanhtrung.msv_19502701.changemaker";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ app/src/main/java/com/dong_pham/phamhavandong_19500311_changemaker/MainActivity.kt
@@ -0,0 +1,115 @@
+package com.dong_pham.phamhavandong_19500311_changemaker
+
+
+
+import android.icu.util.UniversalTimeScale.toBigDecimal
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.math.RoundingMode
+
+class MainActivity : AppCompatActivity() {
+    private val MAX_PRICE: Double = 20 * 9999.0
+    private val magicNumber: Int = 6
+    private lateinit var prices: Array<TextView>
+    private lateinit var newPrice: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        prices = arrayOf(
+            qty20dollars, qty10dollars,
+            qty5dollars, qty1dollar,
+            qty25cents, qty10cents,
+            qty5cents, qty1cent
+        )
+
+        newPrice = if (
+            currAmount.text.toString() == "0.00"
+        ) "" else currAmount.text.toString()
+
+        /* function that handles when a button is pressed */
+        fun onButtonClicked(btn: TextView) {
+            /* default value = -1 is clear button */
+            var value: Int = -1
+
+            /* change value when button is not clear button */
+            if (btn != btnClear) {
+                value = btn.text.toString().toInt()
+            }
+
+            /* clear button is pressed */
+            if (value == -1) {
+                newPrice = ""
+                currAmount.text = "0.00"
+                prices.forEach { it -> it.text = "0" }
+                return
+            }
+
+            /* change current price */
+            newPrice += value.toString()
+            newPrice = newPrice.toInt().toString()
+
+            var parsedNumber = newPrice.toDouble() / 100.0
+
+            /* current amount is too big */
+            if (newPrice != "") {
+                if (parsedNumber > MAX_PRICE) {
+                    Toast.makeText(this, getString(R.string.alertBigNum), Toast.LENGTH_SHORT).show()
+                    return
+                }
+            }
+
+            currAmount.text = parsedNumber.toString()
+
+            val listPrices = arrayOf(20.0, 10.0, 5.0, 1.0, 0.25, 0.1, 0.05, 0.01)
+            for (i in listPrices.indices) {
+                var amounts = (parsedNumber / listPrices[i]).toInt()
+                prices[i].text = amounts.toString()
+                parsedNumber -= amounts.toDouble() * listPrices[i]
+                parsedNumber = parsedNumber.toBigDecimal().setScale(magicNumber, RoundingMode.UP).toDouble()
+            }
+        }
+
+        /* array of button */
+        arrayOf<TextView>(
+            btn1, btn2, btn3,
+            btn4, btn5, btn6,
+            btn7, btn8, btn9,
+            btn0, btnClear
+        ).forEach { it -> it.setOnClickListener() {
+            onButtonClicked(it as TextView)
+        }}
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        var currPrice: TextView = currAmount
+        var oldPrices: ArrayList<String> = ArrayList()
+
+        for (i in prices) {
+            oldPrices.add(i.text.toString())
+        }
+
+        outState.putString("currPrice", currPrice.text.toString())
+        outState.putString("priceprice", newPrice)
+        outState.putStringArrayList("oldPrices", oldPrices)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        currAmount.text = savedInstanceState.getString("currPrice")
+        newPrice = savedInstanceState.getString("priceprice")!!
+        var oldPrices = savedInstanceState.getStringArrayList("oldPrices")
+
+        for (i in prices.indices) {
+            prices[i].text = oldPrices?.get(i) ?: "0"
+        }
+    }
+}
